2efce5885a3e7165cd3169a1ba18d47207373020
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-12 23:52:13 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -3,22 +3,21 @@ sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --use
 # sfdx force:org:create -f config/project-scratch-def.json --setalias archive-data --setdefaultusername
 
 # Push the security settings first, otherwise the push will fail.
-sfdx force:source:deploy -m Settings:Security
+sf project deploy start -m Settings:Security
 
 # Push the metadata into the new scratch org.
-sfdx force:source:push
+sf project deploy start
 
 # Assign user the permset
-sfdx force:user:permset:assign -n ArchiveData
+sf org assign permset -n ArchiveData
 
 # Set the default password.
-sfdx shane:user:password:set -g User -l User -p salesforce1
+sf demoutil user password set -p salesforce1 -g User -l User
 
 # Open the org.
-sfdx force:org:open
-
+sf org open
 # Update the dates in our demo data relative to the original data generation date.
 sfdx shane:data:dates:update -r 5-20-2020
 
 # Import the data required by the demo
-sfdx automig:load --inputdir ./data --deletebeforeload
+sf automig load --inputdir ./data --deletebeforeload
